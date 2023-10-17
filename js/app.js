847fd172ba7d8f994a4e1099775445cb40f383ab
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2023-10-16 21:38:30 -0700

    Add tokyo object

--- js/app.js
+++ js/app.js
@@ -10,3 +10,16 @@ const seattle = {
     return Math.floor(Math.random() * (max-min) + min);
   },
 };
+
+const tokyo = {
+  minHourlyCustomers: 3,
+  maxhourlyCustomer: 24,
+  cookieAvg: 1.2,
+  projectedSales: [],
+  generateCustomers: function() {
+    let min = Math.ceil(this.minHourlyCustomers);
+    let max = Math.floor(this.maxhourlyCustomer);
+
+    return Math.floor(Math.random() * (max-min) + min);
+  },
+};
