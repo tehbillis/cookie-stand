1066db52b4572707d4d7824d52c87d93dc6453e7
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2023-10-20 23:29:55 -0700

    Add location name to table row and clean up commented code

--- js/app.js
+++ js/app.js
@@ -22,26 +22,27 @@ class Location {
     let cookiesTotal = document.createElement('td');
     let salesTotal = 0;
 
+    // Add the location name to the table row
+    salesProjection.appendChild(document.createElement('td')).innerText = this.name;
+
+    // Add sales data to the table row
     for (let i = 6; i <=20; i++) {
       let cookiesSold = Math.trunc(this.generateCustomers() * this.cookieAvg);
       let hourlyCookies = document.createElement('td');
       salesTotal += cookiesSold;
 
       if (i < 12) {
-        //salesProjection.push(i + 'am: ' + cookiesSold + ' cookies');
         hourlyCookies.innerText = i + 'am: ' + cookiesSold + ' cookies';
       } else if (i === 12) {
-        //salesProjection.push(i + 'pm: ' + cookiesSold + ' cookies');
         hourlyCookies.innerText = i + 'pm: ' + cookiesSold + ' cookies';
       } else {
-        //salesProjection.push((i-12) + 'pm: ' + cookiesSold + ' cookies');
         hourlyCookies.innerText = (i-12) + 'pm: ' + cookiesSold + ' cookies';
       }
 
       salesProjection.appendChild(hourlyCookies);
     }
 
-    //salesProjection.push('Total: ' + salesTotal + ' cookies');
+    // Add total to table row
     salesProjection.appendChild(cookiesTotal).innerText = 'Total: ' + salesTotal + ' cookies';
 
     console.log(salesProjection);
