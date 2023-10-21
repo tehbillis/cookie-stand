5c6ab6d18343dba1781bc527192dd7a6ba97aaa8
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2023-10-20 21:35:11 -0700

    Replace object literals with a class

--- js/app.js
+++ js/app.js
@@ -1,95 +1,49 @@
-const seattle = {
-  minHourlyCustomers: 23,
-  maxhourlyCustomer: 65,
-  cookieAvg: 6.3,
-  projectedSales: [],
-  generateCustomers: function() {
-    let min = Math.ceil(this.minHourlyCustomers);
-    let max = Math.floor(this.maxhourlyCustomer);
+const locations = [];
 
-    return Math.floor(Math.random() * (max-min) + min);
-  },
-};
-
-const tokyo = {
-  minHourlyCustomers: 3,
-  maxhourlyCustomer: 24,
-  cookieAvg: 1.2,
-  projectedSales: [],
-  generateCustomers: function() {
-    let min = Math.ceil(this.minHourlyCustomers);
-    let max = Math.floor(this.maxhourlyCustomer);
-
-    return Math.floor(Math.random() * (max-min) + min);
-  },
-};
-
-const dubai = {
-  minHourlyCustomers: 11,
-  maxhourlyCustomer: 38,
-  cookieAvg: 3.7,
-  projectedSales: [],
-  generateCustomers: function() {
-    let min = Math.ceil(this.minHourlyCustomers);
-    let max = Math.floor(this.maxhourlyCustomer);
-
-    return Math.floor(Math.random() * (max-min) + min);
-  },
-};
-
-const paris = {
-  minHourlyCustomers: 20,
-  maxhourlyCustomer: 38,
-  cookieAvg: 2.3,
-  projectedSales: [],
-  generateCustomers: function() {
-    let min = Math.ceil(this.minHourlyCustomers);
-    let max = Math.floor(this.maxhourlyCustomer);
-
-    return Math.floor(Math.random() * (max-min) + min);
-  },
-};
-
-const lima = {
-  minHourlyCustomers: 2,
-  maxhourlyCustomer: 16,
-  cookieAvg: 4.6,
-  projectedSales: [],
-  generateCustomers: function() {
-    let min = Math.ceil(this.minHourlyCustomers);
-    let max = Math.floor(this.maxhourlyCustomer);
-
-    return Math.floor(Math.random() * (max-min) + min);
-  },
-};
-
-// Function to calculate the projected sales of a location
-
-function calculateSales (city) {
-  let salesProjection = [];
-  let salesTotal = 0;
-
-  for (let i = 6; i <=20; i++) {
-    let cookiesSold = Math.trunc(city.generateCustomers() * city.cookieAvg);
-
-    salesTotal += cookiesSold;
-
-    if (i < 12) {
-      salesProjection.push(i + 'am: ' + cookiesSold + ' cookies');
-    } else if (i === 12) {
-      salesProjection.push(i + 'pm: ' + cookiesSold + ' cookies');
-    } else {
-      salesProjection.push((i-12) + 'pm: ' + cookiesSold + ' cookies');
-    }
+class Location {
+  constructor (min, max, avg) {
+    this.minHourlyCustomers = min;
+    this.maxhourlyCustomers = max;
+    this.cookieAvg = avg;
+    this.projectedSales = this.calculateSales();
+    locations.push(this);
   }
 
-  salesProjection.push('Total: ' + salesTotal + ' cookies');
+  generateCustomers() {
+    let min = Math.ceil(this.minHourlyCustomers);
+    let max = Math.floor(this.maxhourlyCustomers);
 
-  return salesProjection;
+    return Math.floor(Math.random() * (max-min) + min);
+  }
+
+  calculateSales () {
+    let salesProjection = [];
+    let salesTotal = 0;
+
+    for (let i = 6; i <=20; i++) {
+      let cookiesSold = Math.trunc(this.generateCustomers() * this.cookieAvg);
+
+      salesTotal += cookiesSold;
+
+      if (i < 12) {
+        salesProjection.push(i + 'am: ' + cookiesSold + ' cookies');
+      } else if (i === 12) {
+        salesProjection.push(i + 'pm: ' + cookiesSold + ' cookies');
+      } else {
+        salesProjection.push((i-12) + 'pm: ' + cookiesSold + ' cookies');
+      }
+    }
+
+    salesProjection.push('Total: ' + salesTotal + ' cookies');
+
+    return salesProjection;
+  }
 }
 
-seattle.projectedSales = calculateSales(seattle);
-tokyo.projectedSales = calculateSales(tokyo);
-dubai.projectedSales = calculateSales(dubai);
-paris.projectedSales = calculateSales(paris);
-lima.projectedSales = calculateSales(lima);
+const seattle = new Location(23, 65, 6.3);
+const tokyo = new Location(3, 24, 1.2);
+const dubai = new Location(11, 38, 3.7);
+const paris = new Location(20, 38, 2.3);
+const lima = new Location(2, 16, 4.6);
+
+console.log(locations);
